4509517566859a04eccf5aadcdc62a55c71e7760
losferwords <losfer@list.ru>
2021-08-04 20:46:23 +0300

    Fix battle end

--- src/app/static/const.ts
+++ src/app/static/const.ts
@@ -24,6 +24,6 @@ export class Const {
   public static apiLearnAbility = 'learn-ability';
   public static apiBotAction = 'bot-action';
 
-  public static availableHeroes = ['paragon', 'highlander', 'druid', 'oracle'];
-  public static botThinkTime = 300000;
+  public static availableHeroes = ['paragon', 'highlander', 'druid', 'oracle', 'avatar'];
+  public static botThinkTime = 120000;
 }
